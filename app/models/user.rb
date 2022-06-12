50e9e779cafca9251272a43f00c6af230c74cbb5
Bao Nguyen <ntbao.me@gmail.com>
2022-06-12 09:42:01 +0700

    validate for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :trackable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
